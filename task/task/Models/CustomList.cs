d19d8165c51345d26b2239c7d6f63fb09a5568f7
SabinaNovruzova <tu7cldm19@code.edu.az>
2022-03-30 12:28:18 +0400

    first commit

---
+++ task/task/Models/CustomList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task.Models
+{
+    class CustomList<T> 
+    {
+        private T[] _list;
+        public CustomList()
+        {
+            _list = new T[0];
+        }
+        public CustomList(params T[] nums)
+        {
+            _list = new T[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                _list[i] = nums[i];
+            }
+        }
+        public void Add (T input)
+        {
+            Array.Resize(ref _list, _list.Length + 1);
+            _list[_list.Length - 1] = input;
+        }
+        public void Show()
+        {
+            foreach (var item in _list)
+            {
+                Console.WriteLine(item);
+            }
+        }              
+    }
+}
---
+++ task/task/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(20);
+            numbers.Add(30);
+            numbers.Add(40);
+            numbers.Remove(3);
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+            numbers.Clear();
+            Console.WriteLine(numbers[2]);
+            Console.WriteLine($"Count:{numbers.Count}");
+        }
+    }
+}
